fa30cf2336d1336cca73b73c0ad0ed4b6f22f453
camillehe1992 <camille.he@outlook.com>
2026-07-15 17:25:00 +0800

    chore: rename workflow step and separate terraform resources

--- terraform/source/units/api/lambda_functions.tf
+++ terraform/source/units/api/lambda_functions.tf
@@ -1,17 +1,3 @@
-# Lambda Layer Version that contains external dependencies
-data "local_file" "dependencies_zip_file" {
-  filename = var.dependencies_layer_file_path
-}
-
-resource "aws_lambda_layer_version" "this" {
-  layer_name               = "${local.resource_prefix}-dependencies"
-  description              = "External dependencies for Lambda function"
-  compatible_architectures = [var.architecture]
-  compatible_runtimes      = [var.lambda_runtime]
-  filename                 = data.local_file.dependencies_zip_file.filename
-  source_code_hash         = data.local_file.dependencies_zip_file.content_base64sha256
-}
-
 # Lambda Function that invoked by API Gateway
 module "portal_function" {
   source = "../../modules/lambda_function"
